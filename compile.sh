392fc589d71803cc64d32ef7ebf34c210c2da2da
Kyle Rodgers <kylerodgers@Kyles-MacBook-Pro.local>
2014-07-10 16:42:41 -0500

    Changed compile script

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 rm -r output_data
-g++ -c Engine.cc
-g++ -c Node.cc
-g++ -c PetersenGraph.cc
-g++ -c CompleteGraph.cc
-g++ -c Hypercube2.cc
-g++ -c Hypercube3.cc
-g++ -c Hypercube4.cc
-g++ -c Token.cc
-g++ -c RandomEngine.cc
-g++ *.o main.cc -o main
+g++ -std=c++11 -c Engine.cc
+g++ -std=c++11 -c Node.cc
+g++ -std=c++11 -c PetersenGraph.cc
+g++ -std=c++11 -c CompleteGraph.cc
+g++ -std=c++11 -c Hypercube2.cc
+g++ -std=c++11 -c Hypercube3.cc
+g++ -std=c++11 -c Hypercube4.cc
+g++ -std=c++11 -c Token.cc
+g++ -std=c++11 -c RandomEngine.cc
+g++ -std=c++11 *.o main.cc -o main
 mkdir -p output_data
