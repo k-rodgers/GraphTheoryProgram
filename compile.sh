a57b553857237f2282e8eaaf8904b1b9e7d6f97d
Kyle Rodgers <kyle.rodgers@nimbelink.com>
2020-12-30 16:09:37 -0600

    compile: Fixed the compile script

--- compile.sh
+++ compile.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e
+
 INCLUDE="-I../include/"
 
 #rm -r output_data
-rm -r build
-rm gtp
+rm -r build && true
+rm gtp && true
 mkdir -p build
 pushd build > /dev/null
 for x in $(ls ../src); do
     #g++ ${INCLUDE} -std=c++17 -c ../src/$x
-    COMMAND="g++ ${INCLUDE} -std=c++17 -luuid -c ../src/$x"
+    COMMAND="g++ ${INCLUDE} -std=c++17 -c ../src/$x -luuid"
     echo $COMMAND
     eval ${COMMAND}
 done
 rm main.o
 popd > /dev/null
-g++ -I./include/ -std=c++17 -g build/*.o src/main.cpp -o gtp
+g++ -I./include/ -std=c++17 -g build/*.o src/main.cpp -luuid -o gtp
 mkdir -p output_data
