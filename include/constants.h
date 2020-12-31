7e75d92253be4a26810d671688795bbb27e74f14
Kyle Rodgers <kyle.rodgers@nimbelink.com>
2020-12-30 20:48:29 -0600

    constants: Added constants file.

---
+++ include/constants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ctime>
+
+namespace Seconds
+{
+    static constexpr int Day = 86400;
+    static constexpr int Hour = 3600;
+    static constexpr int Minute = 60;
+}
+
+namespace Hours
+{
+    static constexpr int Day = 24;
+}
